e579cb143f3f782f764f5ebe700b091aa1515613
Magdalena Kavka <magdalenakavka9@gmail.com>
2026-04-15 19:52:46 +0200

    Delete app directory

--- app/src/main/java/com/kavkamagdalena/bookshelf/ui/theme/ListScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.kavkamagdalena.bookshelf.ui.theme
-
-import androidx.compose.runtime.Composable
-
-
-@Composable
-fun ListScreen(onItemClick: (MyData) -> Unit) {
-    MyItemList(onItemClick = onItemClick)
-}
--- app/src/main/java/com/kavkamagdalena/bookshelf/ui/theme/MyData.kt
+++
@@ -1,8 +0,0 @@
-package com.kavkamagdalena.bookshelf.ui.theme
-
-data class MyData (
-    val ID : String,
-    val title : String,
-    val description : String
-)
-
